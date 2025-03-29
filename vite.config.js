9e3130ae9f615951752ff13992146e4b1dcb6e92
Wildsberg <harunkurt1@hotmail.com>
2025-03-29 19:38:32 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  root: '.', // <-- explizit das Root-Verzeichnis angeben
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html') // <-- explizit index.html setzen
+    }
+  }
 })
